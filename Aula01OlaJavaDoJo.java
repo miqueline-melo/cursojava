e5169dacbbb470d1f5efdf72e8382d2766afba29
miqueline-melo <miquelinepereiramelo@gmail.com>
2023-01-02 18:26:43 -0300

    aula01 introducao java

---
+++ Aula01OlaJavaDoJo.java
@@ -0,0 +1,12 @@
+package cursodevdojo.maratonajava.introducao;
+
+/**
+ * isso e um comentario <b>javadoc</b>
+ * */
+public class Aula01OlaJavaDoJo {
+    public static void main(String[] args) {
+        {
+            System.out.println("Ola mundo");
+        }
+    }
+}
