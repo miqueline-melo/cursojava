725e2a55910182675762fe91814ce1b59109ebb4
miqueline-melo <miquelinepereiramelo@gmail.com>
2023-01-03 16:57:34 -0300

    aula03 tipos primitivos exercicios

---
+++ Aula03TiposPrimitivosExercicio.java
@@ -0,0 +1,17 @@
+package cursodevdojo.maratonajava.introducao;
+
+
+public class Aula03TiposPrimitivosExercicio {
+    public static void main(String[] args) {
+        String nome = "miqueline";
+        String endereco = "rua das flores, 125, Fortaleza, ceara";
+        double salario = 2500.00;
+        String dataRecebimentoSalario = "15/10/2022";
+
+        String relatorio = "Eu "+ nome + ", morando no endereço, " +endereco + ", confirmo que recebi o salário de " +salario + ", na data " + dataRecebimentoSalario;
+        System.out.println("Eu "+ nome + ", morando no endereço, " +endereco + ", confirmo que recebi o salário de " +salario + ", na data " + dataRecebimentoSalario);
+        System.out.println(relatorio);
+
+
+    }
+}
