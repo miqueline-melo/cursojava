4c535ab82e2a56269730b984715913f674725607
miqueline-melo <miquelinepereiramelo@gmail.com>
2023-01-04 18:25:09 -0300

    aula06 estruturas de repeticao03

---
+++ Aula06EstruturasDeRepeticao03.java
@@ -0,0 +1,16 @@
+package cursodevdojo.maratonajava.introducao;
+
+public class Aula06EstruturasDeRepeticao03 {
+    public static void main(String[] args) {
+        int valor=50;
+
+        for (int i = 0; i < 50; i++) {
+            if(i>25){
+                break;
+            }
+
+            System.out.println(i);
+            }
+
+        }
+    }
