2f4a5d9a815efa8482b27995a92c58c9ed879a5e
miqueline-melo <miquelinepereiramelo@gmail.com>
2023-01-04 18:27:26 -0300

    aula07 arrays01

---
+++ Aula07Arrays01.java
@@ -0,0 +1,17 @@
+package cursodevdojo.maratonajava.introducao;
+
+import java.util.Arrays;
+
+public class Aula07Arrays01 {
+    public static void main(String[] args) {
+        int[] idade = new int[3];
+        idade [0]= 15;
+        idade [1]= 25;
+        idade [2]= 35;
+        System.out.println(idade[0]);
+        System.out.println(idade[1]);
+        System.out.println(idade[2]);
+
+
+    }
+}
